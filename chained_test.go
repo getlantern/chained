207f118d01fc710ea2b3207aab05a7c559e59db8
Ox Cart <ox.to.a.cart@gmail.com>
2014-12-02 20:13:21 -0600

    Using proxytest library

--- chained_test.go
+++ chained_test.go
@@ -1,16 +1,10 @@
 package chained
 
 import (
-	"io"
 	"net"
 	"testing"
 
-	"github.com/getlantern/testify/assert"
-)
-
-var (
-	ping = []byte("ping")
-	pong = []byte("pong")
+	"github.com/getlantern/proxytest"
 )
 
 func TestSuccessNotPipelined(t *testing.T) {
@@ -22,37 +16,11 @@ func TestSuccessPipelined(t *testing.T) {
 }
 
 func doTest(t *testing.T, pipelined bool) {
-	// Set up listener for server endpoint
-	sl, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		t.Fatalf("Unable to listen: %s", err)
-	}
-
 	// Set up listener for server proxy
 	pl, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("Unable to listen: %s", err)
 	}
-
-	// Server that responds to ping
-	go func() {
-		conn, err := sl.Accept()
-		if err != nil {
-			t.Fatalf("Unable to accept connection: %s", err)
-			return
-		}
-		defer conn.Close()
-		b := make([]byte, 4)
-		_, err = io.ReadFull(conn, b)
-		if err != nil {
-			t.Fatalf("Unable to read from client: %s", err)
-		}
-		assert.Equal(t, ping, b, "Didn't receive correct ping message")
-		_, err = conn.Write(pong)
-		if err != nil {
-			t.Fatalf("Unable to write to client: %s", err)
-		}
-	}()
 
 	// upstream proxy server
 	s := &Server{
@@ -70,21 +38,5 @@ func doTest(t *testing.T, pipelined bool) {
 		return net.Dial(pl.Addr().Network(), pl.Addr().String())
 	})
 
-	conn, err := dial(sl.Addr().Network(), sl.Addr().String())
-	if err != nil {
-		t.Fatalf("Unable to dial via proxy: %s", err)
-	}
-	defer conn.Close()
-
-	_, err = conn.Write(ping)
-	if err != nil {
-		t.Fatalf("Unable to write to server via proxy: %s", err)
-	}
-
-	b := make([]byte, 4)
-	_, err = io.ReadFull(conn, b)
-	if err != nil {
-		t.Fatalf("Unable to read from server: %s", err)
-	}
-	assert.Equal(t, pong, b, "Didn't receive correct pong message")
+	proxytest.Go(t, dial)
 }
